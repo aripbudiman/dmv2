ec5e7327cf16409f640e4db72e5deebfbe527d15
aripbudiman <muhamadarifbudiman22@gmail.com>
2022-07-11 21:35:39 +0700

    merapihkan sidebar dan layouts

--- app/Views/layouts/sidebar-menu.php
+++ app/Views/layouts/sidebar-menu.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= base_url('/'); ?>" class="brand-link">
-        <img src="<?= base_url(); ?>/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Manonjaya Empire</span>
+        <img src="<?= base_url(); ?>/img/logo1.png" alt="DMPRINTING Logo" class="brand-image">
+        <span class="brand-text font-weight-light"></span>
     </a>
 
     <!-- Sidebar -->
@@ -25,30 +25,8 @@
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
-                            <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="./index.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v1</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="./index2.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v2</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="./index3.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v3</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
                 <li class="nav-item">
                     <a href="pages/widgets.html" class="nav-link">
                         <i class="nav-icon fas fa-users"></i>
@@ -57,12 +35,12 @@
                         </p>
                     </a>
                 </li>
-                <li class="nav-header">STOKBARANG</li>
+                <li class="nav-header">Input</li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-box"></i>
+                        <i class="nav-icon fas fa-shopping-cart"></i>
                         <p>
-                            Data Master
+                            Pesanan
                             <i class="fas fa-angle-left right"></i>
                             <!-- <span class="badge badge-info right">6</span> -->
                         </p>
@@ -71,26 +49,20 @@
                         <li class="nav-item">
                             <a href="<?= base_url(); ?>/data_barang" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Data Barang</p>
+                                <p>Input Pesanan</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="<?= base_url(); ?>/satuan" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Satuan</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="<?= base_url(); ?>/jenis" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Jenis Barang</p>
+                                <p>List Pesanan</p>
                             </a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-exchange"></i>
+                        <i class="nav-icon fas fa-money-bill"></i>
                         <p>
                             Transaksi
                             <i class="fas fa-angle-left right"></i>
@@ -101,13 +73,13 @@
                         <li class="nav-item">
                             <a href="pages/layout/top-nav.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Barang Masuk</p>
+                                <p>Payment</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Barang Keluar</p>
+                                <p>Down Payment</p>
                             </a>
                         </li>
                     </ul>
@@ -136,12 +108,12 @@
                         </li>
                     </ul>
                 </li>
-                <li class="nav-header">PROYEKSI</li>
+                <li class="nav-header">Konfigurasi</li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-circle"></i>
+                        <i class="nav-icon fas fa-gear"></i>
                         <p>
-                            Level 1
+                            Ukuran
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
@@ -149,7 +121,43 @@
                         <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Level 2</p>
+                                <p>Tipe</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Bahan</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Lebar</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Finishing</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-header">Laporan</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-circle"></i>
+                        <p>
+                            Pendapatan
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Pendatan Tipe</p>
                             </a>
                         </li>
                         <li class="nav-item">
@@ -189,14 +197,6 @@
                         </li>
                     </ul>
                 </li>
-                <li class="nav-header">CASHOPNAME</li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon far fa-circle text-danger"></i>
-                        <p class="text">Important</p>
-                    </a>
-                </li>
-                <li class="nav-header">LAPORAN</li>
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
