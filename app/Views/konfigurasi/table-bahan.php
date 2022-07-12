fe98a0b3d5edd429c9ca11ced80105144b735ef7
aripbudiman <muhamadarifbudiman22@gmail.com>
2022-07-12 22:16:00 +0700

    v4

---
+++ app/Views/konfigurasi/table-bahan.php
@@ -0,0 +1,29 @@
+<table class="table table-hover" id="table-bahan">
+    <thead>
+        <tr>
+            <th scope="col">No</th>
+            <th scope="col">Kode Bahan</th>
+            <th scope="col">Nama Bahan</th>
+            <th scope="col">Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $no = 1;
+        foreach ($bahan as $b) : ?>
+            <tr>
+                <th scope="row"><?= $no++; ?></th>
+                <td><?= $b['kode_bahan']; ?></td>
+                <td><?= $b['nama_bahan']; ?></td>
+                <td>
+                    <button class="btn btn-sm btn-success edit-bahan"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button class="btn btn-sm btn-danger delete-bahan" data-id="<?= $b['id']; ?>"><i class="fa-solid fa-trash"></i></button>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<script>
+    $(document).ready(function() {
+        $('#table-bahan').dataTable()
+    });
+</script>
