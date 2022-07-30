4be3e68201b7783d2fcaf22c0861b743c1f4746d
aripbudiman <muhamadarifbudiman22@gmail.com>
2022-07-30 17:13:10 +0700

    bill

--- app/Controllers/Payment.php
+++ app/Controllers/Payment.php
@@ -10,6 +10,8 @@ use App\Models\TmpPesananModel;
 use App\Models\IsijurnalModel;
 use App\Models\JurnalModel;
 use App\Models\Pesananinput;
+use Dompdf\Dompdf;
+
 
 class Payment extends BaseController
 {
@@ -209,5 +211,22 @@ class Payment extends BaseController
         } else {
             exit('maaf tidak bisa dilanjutkan');
         }
+    }
+
+
+    public function getBill()
+    {
+        // instantiate and use the dompdf class
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml('hello world');
+
+        // (Optional) Setup the paper size and orientation
+        $dompdf->setPaper('A4', 'landscape');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser
+        $dompdf->stream($this->filename, array("Attachment" => false));
     }
 }
